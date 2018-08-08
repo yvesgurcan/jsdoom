6250fe33ac30ee2e21c8f107a8fffc321f94502d
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-07 23:16:37 -0700

    move bindKeys

---
+++ client/src/engine/bindKeys.js
@@ -0,0 +1,41 @@
+import store from './store';
+const { dispatch } = store;
+
+// bind keyboard events to game functions (movement, etc)
+export default () => {
+	document.onkeydown = function(event) {
+		const { keyCode } = event;
+		switch (keyCode) {
+
+            case 38:
+                dispatch({ type: 'PLAYER_MOVE_FORWARD' });
+				break;
+
+			case 40:
+                dispatch({ type: 'PLAYER_MOVE_BACKWARD' });
+				break;
+
+			case 37:
+                dispatch({ type: 'PLAYER_TURN_LEFT' });
+				break;
+
+			case 39:
+                dispatch({ type: 'PLAYER_TURN_RIGHT' });
+				break;
+		}
+	}
+
+	document.onkeyup = function(event) {
+		const { keyCode } = event;
+		switch (keyCode) {
+			case 38:
+            case 40:
+                dispatch({ type: 'PLAYER_MOVE_STOP' });
+				break;
+			case 37:
+			case 39:
+                dispatch({ type: 'PLAYER_TURN_STOP' });
+				break;
+		}
+	}
+}
