a8c3d2739503f22abbae2bdddb30cbbf3094fde9
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-07 22:31:34 -0700

    store

---
+++ client/src/engine/store.js
@@ -0,0 +1,8 @@
+import { createStore } from 'redux'
+import reducers from './reducers/';
+
+export default createStore(
+    reducers, /* preloadedState, */
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+ 
