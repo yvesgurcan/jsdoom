19ba3aa84e54e5fc254ff6ccfc32a99bce685a22
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-08 00:39:33 -0700

    screen size reducer

--- client/src/engine/reducers/screen.js
+++ client/src/engine/reducers/screen.js
@@ -1,3 +1,8 @@
+import {
+    stripWidth,
+    fov,
+} from '../constants';
+
 const initState = {
     strips: [],
 };
@@ -6,6 +11,8 @@ export default (prevState = initState, action) => {
     const {
         type,
         strips,
+        width,
+        height,
     } = action;
     const nextState = { ...prevState };
 
@@ -14,6 +21,17 @@ export default (prevState = initState, action) => {
             return {
                 ...prevState,
                 strips,
+            };
+        }
+        case 'SCREEN_RESIZE': {
+            const numRays = Math.ceil(width / stripWidth);
+            const viewDist = (width / 2) / Math.tan((fov / 2));
+            return {
+                ...prevState,
+                width,
+                height,
+                numRays,
+                viewDist,
             };
         }
         default: {
