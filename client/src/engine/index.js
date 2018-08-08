5b8a0d4eff181a06753fd27c2a07a173b48dcd31
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-07 23:22:06 -0700

    move initScreen to its own file

--- client/src/engine/index.js
+++ client/src/engine/index.js
@@ -1,5 +1,5 @@
 import $ from './getElementById';
-import dc from './createElement';
+import initScreen from './initScreen';
 import bindKeys from './bindKeys';
 import {
     map,
@@ -24,37 +24,6 @@ function init() {
 	initScreen();
 	drawMiniMap();
 	gameLoop();
-}
-
-
-function initScreen() {
-	let screen = $("screen");
-
-    let strips = [];
-	for (let i = 0; i < screenWidth; i += stripWidth) {
-		let strip = dc("div");
-		strip.style.position = "absolute";
-		strip.style.left = i + "px";
-		strip.style.width = stripWidth+"px";
-		strip.style.height = "0px";
-		strip.style.overflow = "hidden";
-
-		strip.style.backgroundColor = "magenta";
-
-		var img = new Image();
-		img.src = (window.opera ? "walls-19-colors.png" : "walls.png");
-		img.style.position = "absolute";
-		img.style.left = "0px";
-
-        strip.appendChild(img);
-        // assign the image to a property on the strip element so we have easy access to the image later
-		strip.img = img;	
-
-		strips.push(strip);
-        screen.appendChild(strip);
-    }
-    dispatch({ type: 'SCREEN_SET_STRIPS', strips });
-
 }
 
 function gameLoop() {
---
+++ client/src/engine/initScreen.js
@@ -0,0 +1,38 @@
+import getElementById from './getElementById';
+import createElement from './createElement';
+import {
+    screenWidth,
+    stripWidth,
+} from './constants';
+import store from './store';
+const { dispatch } = store;
+
+export default () => {
+	let screen = getElementById('screen');
+
+    let strips = [];
+	for (let i = 0; i < screenWidth; i += stripWidth) {
+		let strip = createElement('div');
+		strip.style.position = 'absolute';
+		strip.style.left = i + 'px';
+		strip.style.width = stripWidth+'px';
+		strip.style.height = '0px';
+		strip.style.overflow = 'hidden';
+
+		strip.style.backgroundColor = 'magenta';
+
+		var img = new Image();
+		img.src = (window.opera ? 'walls-19-colors.png' : 'walls.png');
+		img.style.position = 'absolute';
+		img.style.left = '0px';
+
+        strip.appendChild(img);
+        // assign the image to a property on the strip element so we have easy access to the image later
+		strip.img = img;	
+
+		strips.push(strip);
+        screen.appendChild(strip);
+    }
+    dispatch({ type: 'SCREEN_SET_STRIPS', strips });
+
+}
