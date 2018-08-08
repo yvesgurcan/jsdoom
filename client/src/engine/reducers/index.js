a0f1bfb36ad18e57cb33207c45778db227500fe1
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-07 23:16:58 -0700

    screen reducer

--- client/src/engine/reducers/index.js
+++ client/src/engine/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "../../../../node_modules/redux";
 import player from './player';
+import screen from './screen';
 
 export default combineReducers({
     player,
+    screen,
 });
---
+++ client/src/engine/reducers/screen.js
@@ -0,0 +1,26 @@
+const initState = {
+    strips: [],
+};
+
+export default (prevState = initState, action) => {
+    const {
+        type,
+        strips,
+    } = action;
+    const nextState = { ...prevState };
+
+    switch(type) {
+        case 'SCREEN_SET_STRIPS': {
+            return {
+                ...prevState,
+                strips,
+            };
+        }
+        default: {
+            return nextState;
+        }
+    }
+
+    return nextState;
+
+}
