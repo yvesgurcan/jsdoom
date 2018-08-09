efb0efecb599e97bcf71f02eef363b3307e8f3ee
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-09 00:46:15 -0700

    fix redux imports

--- client/src/engine/reducers/index.js
+++ client/src/engine/reducers/index.js
@@ -1,4 +1,4 @@
-import { combineReducers } from "../../../../node_modules/redux";
+import { combineReducers } from "redux";
 import player from './player';
 import screen from './screen';
 
--- client/src/engine/store.js
+++ client/src/engine/store.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux'
-import reducers from './reducers/';
+import reducers from './reducers';
 
 export default createStore(
     reducers, /* preloadedState, */
