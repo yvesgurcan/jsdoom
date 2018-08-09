c8e97ccc303e92c1e1fd40a62eeccc43046b7fe1
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-09 00:47:43 -0700

    unnecessary constant

--- client/src/engine/constants.js
+++ client/src/engine/constants.js
@@ -1,12 +1,10 @@
 export const miniMapScale = 8;
 
-export const stripWidth = 1;
+export const stripWidth = 2;
 export const fov = 60 * Math.PI / 180;
 export const fovHalf = fov / 2;
 
 export const twoPI = Math.PI * 2;
-
-export const numTextures = 4;
 
 export const assetFolder = '/client/assets';
 export const textureFolder = `${assetFolder}/textures`;
