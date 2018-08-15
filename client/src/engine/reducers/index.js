8094db680a00254193b932b410e74354a4155978
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-14 22:59:32 -0700

    add new reducers

--- client/src/engine/reducers/index.js
+++ client/src/engine/reducers/index.js
@@ -1,12 +1,18 @@
 import { combineReducers } from 'redux';
 import wallMap from './wallMap';
+import player from './player';
+import enemyTypes from './enemyTypes';
+import enemyMap from './enemyMap';
 import decorationTypes from './decorationTypes';
-import decorationMapInit from './decorationMapInit';
 import decorationMap from './decorationMap';
+import decorationMapPlacement from './decorationMapPlacement';
 
 export default combineReducers({
     wallMap,
+    player,
+    enemyTypes,
+    enemyMap,
     decorationTypes,
-    decorationMapInit,
     decorationMap,
+    decorationMapPlacement,
 });
