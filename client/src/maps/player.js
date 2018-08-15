0bb54e42049f4516e6c50a86aeace81a664f7da0
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-15 00:22:28 -0700

    update init coordinates of player

--- client/src/maps/player.js
+++ client/src/maps/player.js
@@ -1,8 +1,8 @@
 export default {
-	x: 10.5,		// current x, y position
-	y: 6.5,
+	x: 27,
+	y: 7,
 	dir: 0,		// the direction that the player is turning, either -1 for left or 1 for right.
-	rotDeg: 0,		// the current angle of rotation 
+	rotDeg: 90,
 	rot: 0,		// rotation in radians
 	speed: 0,		// is the playing moving forward (speed = 1) or backwards (speed = -1).
 	moveSpeed: 0.10,	// how far (in map units) does the player move each step/update
