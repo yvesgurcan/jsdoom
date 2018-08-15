7bae2818c00409423165912f02424992bb32a1b7
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-14 20:20:46 -0700

    doom decoration types

--- client/src/types/decorations.js
+++ client/src/types/decorations.js
@@ -1,6 +1,13 @@
-export default [
-	{ img: 'table-chairs.png', block: true },	// 0
-	{ img: 'armor.png', block: true },		// 1
-	{ img: 'plantgreen.png', block: true },	// 2
-	{ img: 'lamp.png', block: false }		// 3
-];
+export default {
+	barrel: { img: 'BAR1A0', block: true },
+	candle: { img: 'CANDA0', block: false },
+	candelabra: { img: 'CBRAA0', block: true },
+	eye: { img: 'CEYEA0', block: true },
+	column1: { img: 'COL1A0', block: true },
+	column2: { img: 'COL2A0', block: true },
+	column3: { img: 'COL3A0', block: true },
+	column4: { img: 'COL4A0', block: true },
+	column5: { img: 'COL5A0', block: true },
+	column6: { img: 'COL6A0', block: true },
+	lamp1: { img: 'COLUA0', block: true },
+};
