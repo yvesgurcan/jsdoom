68d9c2cd4579480407c21b976844cea28287eaf2
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-16 20:16:57 -0700

    renderEnemies

--- client/src/engine/constants.js
+++ client/src/engine/constants.js
@@ -3,3 +3,9 @@ const assetsPath = 'client/assets';
 export const wolfPath = `${assetsPath}/wolf`;
 export const ext = '.png';
 export const decorationPath = `${assetsPath}/decorations`;
+
+export const screenWidth = 320;
+export const screenHeight = 200;
+
+export const fov = (60 * Math.PI) / 180;
+export const viewDist = (screenWidth / 2) / Math.tan((fov / 2));
