1c5e186acf290dd0e6c2d56eb56aa2ffe3933178
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-17 22:00:52 -0700

    handling of strafe movements

--- client/src/engine/move.js
+++ client/src/engine/move.js
@@ -12,27 +12,53 @@ export default (entityType, entity, timeDelta, index) => {
     const updatedEntity = { ...entity };
 
 	const mul = timeDelta / gameCycleDelay;
-
-	const moveStep = mul * updatedEntity.speed * updatedEntity.moveSpeed;	// entity will move this far along the current direction vector
-
-    const rotation = mul * updatedEntity.dir * updatedEntity.rotSpeed;
-
-	updatedEntity.rotDeg += rotation; // add rotation if entity is rotating (entity.dir != 0)
-    updatedEntity.rotDeg %= 360;
-
-	if (updatedEntity.rotDeg < -180) updatedEntity.rotDeg += 360;
-	if (updatedEntity.rotDeg >= 180) updatedEntity.rotDeg -= 360;
-
-	const snap = (updatedEntity.rotDeg + 360) % 90;
-	if (snap < 2 || snap > 88) {
-		updatedEntity.rotDeg = Math.round(updatedEntity.rotDeg / 90) * 90;
-	}
-
-    updatedEntity.rot = (updatedEntity.rotDeg * Math.PI) / 180;
+	
     
-    // calculate new entity position with simple trigonometry
-	const newX = updatedEntity.x + (Math.cos(updatedEntity.rot) * moveStep);	
-	const newY = updatedEntity.y + (Math.sin(updatedEntity.rot) * moveStep);
+    let newX = updatedEntity.x;
+    let newY = updatedEntity.y;
+    let moveStep = 0;
+    let rotation = 0;
+    if (entity.strafe) {
+        let reduceSpeed = 1;
+        if (updatedEntity.dir !== 0 && updatedEntity.speed !== 0) {
+            reduceSpeed = 2;
+        }
+
+        if (updatedEntity.dir !== 0) {
+            moveStep = (mul * updatedEntity.dir * -1 * updatedEntity.moveSpeed * 1.2) / reduceSpeed;
+
+            newX += (Math.cos(updatedEntity.rot - (Math.PI / 2)) * moveStep);
+            newY += (Math.sin(updatedEntity.rot - (Math.PI / 2)) * moveStep);
+        }
+        if (updatedEntity.speed !== 0) {
+            moveStep = (mul * updatedEntity.speed * updatedEntity.moveSpeed) / reduceSpeed;
+            
+            newX += (Math.cos(updatedEntity.rot) * moveStep);
+            newY += (Math.sin(updatedEntity.rot) * moveStep);
+        }
+    } else {
+        // entity will move this far along the current direction vector
+        moveStep = mul * updatedEntity.speed * updatedEntity.moveSpeed;
+
+        rotation = mul * updatedEntity.dir * updatedEntity.rotSpeed;
+
+        updatedEntity.rotDeg += rotation; // add rotation if entity is rotating (entity.dir != 0)
+        updatedEntity.rotDeg %= 360;
+    
+        if (updatedEntity.rotDeg < -180) updatedEntity.rotDeg += 360;
+        if (updatedEntity.rotDeg >= 180) updatedEntity.rotDeg -= 360;
+    
+        const snap = (updatedEntity.rotDeg + 360) % 90;
+        if (snap < 2 || snap > 88) {
+            updatedEntity.rotDeg = Math.round(updatedEntity.rotDeg / 90) * 90;
+        }
+
+        updatedEntity.rot = (updatedEntity.rotDeg * Math.PI) / 180;
+
+        // calculate new entity position with simple trigonometry
+        newX += (Math.cos(updatedEntity.rot) * moveStep);
+        newY += (Math.sin(updatedEntity.rot) * moveStep);
+    }
 
 	const pos = checkCollision(updatedEntity.x, updatedEntity.y, newX, newY, 0.01);
 
