9328c0c723adcb6b32805dcf29c98422110fb344
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-17 22:00:06 -0700

    add constant key definitions

--- client/src/engine/constants.js
+++ client/src/engine/constants.js
@@ -11,3 +11,17 @@ export const fov = (60 * Math.PI) / 180;
 export const stripWidth = 3;
 
 export const twoPI = Math.PI * 2;
+
+export const keys = {
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39,
+    W: 87,
+    S: 83,
+    A: 65,
+    D: 68,
+    V: 86,
+    SHIFT: 16,
+    TAB: 9,
+};
