f9a4f42e577915e23a38fa60d8b1672c4f22641a
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-18 01:42:56 -0700

    list of songs

--- client/src/types/music.js
+++ client/src/types/music.js
@@ -1,3 +1,51 @@
+const doom = {
+    D_E1M1: 'At Doom\'s Gate',
+    D_E1M2: 'The Imp\'s Song',
+    D_E1M3: 'Dark Halls',
+    D_E1M4: 'Kitchen Ace (and Taking Names)',
+    D_E1M5: 'Suspense',
+    D_E1M6: 'On the Hunt',
+    D_E1M7: 'Demons on the Prey',
+    // the MP3 of this song does not sound right...
+    D_E1M8: 'Sign of Evil',
+    D_E1M9: 'Hiding the Secrets',
+    D_E2M1: 'I Sawed the Demons',
+    D_E2M2: 'The Demons From Adrian\'s Pen',
+    D_E2M3: 'Intermission from DOOM',
+    D_E2M4: 'They\'re Going to Get You',
+    D_E2M6: 'Sinister',
+    D_E2M7: 'Waltz of the Demons',
+    D_E2M8: 'Nobody Told Me About id',
+    D_E2M9: 'Untitled',
+    D_E3M2: 'Donna to the Rescue',
+    D_E3M3: 'Deep into the Code',
+    D_E3M8: 'Facing the Spider',
+    D_VICTOR: 'The End of Doom',
+    D_BUNNY: 'Sweet Little Dead Bunny',
+};
+
+const doom2 = {
+    D_RUNNIN: 'Running from Evil',
+    D_STALKS: 'The Healer Stalks',
+    D_COUNTD: 'Countdown to Death',
+    D_BETWEE: 'Between Levels',
+    D_DOOM: 'DOOM',
+    D_THE_DA: 'In the Dark',
+    D_SHAWN: 'Shawn\'s Got the Shotgun',
+    D_DDTBLU: 'The Dave D. Taylor Blues',
+    D_IN_CITY: 'Into Sandy\'s City',
+    D_DEAD: 'The Demon\'s Dead',
+    D_ROMERO: 'Waiting for Romero to Play',
+    D_MESSAGE: 'Message for the Archvile',
+    D_AMPIE: 'Bye Bye American Pie',
+    D_ADRIAN: 'Adrian\'s Asleep',
+    D_TENSE: 'Getting too Tense',
+    D_OPENING: 'Opening to Hell',
+    D_EVIL: 'Evil Incarnate',
+    D_ULTIMA: 'The Ultimate Challenge',
+};
+
 export default {
-    
+    ...doom,
+    ...doom2,
 };
