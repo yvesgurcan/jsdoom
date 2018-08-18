6fd5c36e94572be3568aded38d50d714629c4768
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-18 15:27:10 -0700

    init music on first click

--- client/src/engine/initMusic.js
+++ client/src/engine/initMusic.js
@@ -2,22 +2,24 @@ import songs from '../types/music';
 import { dispatch } from './store';
 
 export default () => {
-    const songNames = Object.keys(songs).map(name => name);
-    const randomIndex = Math.floor(Math.random() * (songNames.length - 1));
-    const songName = songs[songNames[randomIndex]];
-    const songNameFormatted = songName.replace(/ /g, '_').replace(/'/g, '_').replace(/\./g, '_')
+    document.addEventListener('click', () => {
+        const songNames = Object.keys(songs).map(name => name);
+        const randomIndex = Math.floor(Math.random() * (songNames.length - 1));
+        const songName = songs[songNames[randomIndex]];
+        const songNameFormatted = songName.replace(/ /g, '_').replace(/'/g, '_').replace(/\./g, '_')
 
-    const song = new Audio(`/client/assets/music/${songNameFormatted}.mp3`);
+        const song = new Audio(`/client/assets/music/${songNameFormatted}.mp3`);
 
-    const volume = 0.3;
+        const volume = 0.3;
 
-    song.volume = volume;
-    song.loop = true;
+        song.volume = volume;
+        song.loop = true;
 
-    song.play();
-    console.log(`Playing '${songName}'...`);
+        song.play();
+        console.log(`Playing '${songName}'...`);
 
-    window.music = song;
+        window.music = song;
 
-    dispatch({ type: 'SET_MUSIC', payload: { songName, songObject: song, volume } });
+        dispatch({ type: 'SET_MUSIC', payload: { songName, songObject: song, volume } });
+    }, { once: true });
 };
