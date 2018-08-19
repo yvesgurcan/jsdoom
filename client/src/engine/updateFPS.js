fde67e2f4b4ff177bc089051fcd8c879cea3fba3
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-18 23:13:35 -0700

    FPS color changes to orange on automap

--- client/src/engine/updateFPS.js
+++ client/src/engine/updateFPS.js
@@ -2,11 +2,19 @@ import getElementById from './getElementById';
 import { getState } from './store';
 
 export default (timeDelta) => {
-    const { hud: { showFPS } } = getState();
+    const {
+        hud: { showFPS },
+        automap: { showAutomap },
+    } = getState();
     const framerate = getElementById('fps');
     if (showFPS) {
         if (framerate.style.display !== 'flex') {
             framerate.style.display = 'flex';
+        }
+        if (showAutomap && framerate.style.color !== 'orange') {
+            framerate.style.color = 'orange';
+        } else if (!showAutomap && framerate.style.color !== 'red') {
+            framerate.style.color = 'red';
         }
         const fps = 1000 / timeDelta;
         framerate.innerHTML = fps.toFixed(1);
