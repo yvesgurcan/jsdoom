62ca2e60e9533c96355a34ccea34140cb646a22b
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-19 22:19:28 -0700

    check if enemy entities are valid

--- client/src/engine/initEnemies.js
+++ client/src/engine/initEnemies.js
@@ -20,9 +20,28 @@ export default () => {
     for (let i = 0; i < mapEnemies.length; i++) {
         const enemy = { ...mapEnemies[i] };
         const {
+            type,
+            x,
+            y,
+        } = enemy;
+
+        if (!type) {
+            console.error('initEnemies(): Enemy type is undefined. Entity will not be placed on the map.', { entity: enemy });
+            /* eslint-disable-next-line */
+            continue;
+        }
+
+        if (!x || !y) {
+            console.error('initEnemies(): x and/or y coordinates are undefined. Entity will not be placed on the map.', { entity: enemy });
+            /* eslint-disable-next-line */
+            continue;
+        }
+        
+        const enemyType = enemyTypes[enemy.type];
+        const {
             moveSpeed,
             rotSpeed,
-        } = enemy;
+        } = enemyType;
 
         if (!moveSpeed || !rotSpeed) {
             console.error('initEnemies(): moveSpeed and/or rotSpeed is undefined. Entity will not be placed on the map.', { entity: enemy });
@@ -30,40 +49,38 @@ export default () => {
             continue;
         }
         
-        const type = { ...enemyTypes[enemy.type] };
         const id = uuid4();
 
         const img = createElement('img');
         img.id = id;
-        img.src = `${wolfPath}/${type.img}${ext}`;
+        img.src = `${wolfPath}/${enemyType.img}${ext}`;
         img.style.display = 'none';
         img.style.position = 'absolute';
 
-        enemy.id = id;
-        enemy.state = 0;
-        enemy.rot = 0;
-        enemy.rotDeg = 0;
-        enemy.dir = 0;
-        enemy.speed = 0;
-        enemy.moveSpeed = type.moveSpeed;
-        enemy.rotSpeed = type.rotSpeed;
-        enemy.totalStates = type.totalStates;
-
-        enemy.oldStyles = {
-            left: 0,
-            top: 0,
-            width: 0,
-            height: 0,
-            clip: '',
-            display: 'none',
-            zIndex: 0
+        const element = {
+            ...enemy,
+            id,
+            rot: 0,
+            rotDeg: 0,
+            dir: 0,
+            speed: 0,
+            moveSpeed,
+            rotSpeed,
+            oldStyles: {
+                left: 0,
+                top: 0,
+                width: 0,
+                height: 0,
+                clip: '',
+                display: 'none',
+                zIndex: 0
+            },
+            img,
         };
-
-        enemy.img = img;
 
         enemies = [
             ...enemies,
-            enemy,
+            element,
         ];
 
         screen.appendChild(img);
