a49c17dad85e295cb78395526e8d1c7fbd4d80a4
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-20 23:09:13 -0700

    enemyMap reducer

--- client/src/engine/reducers/enemyMap.js
+++ client/src/engine/reducers/enemyMap.js
@@ -4,18 +4,59 @@ const initState = [...enemyMap];
 export default (prevState = initState, action) => {
     const {
         type,
-        payload,
+        payload = {},
         index,
     } = action;
+
+    const {
+        x,
+        y,
+        rotDeg,
+        rot,
+        speed,
+        walkFrame,
+    } = payload;
 
     switch (type) {
         case 'INIT_ENEMY_MAP': {
             return payload;
         }
-        case 'SET_ENEMY_COORDINATES': {
+        case 'UPDATE_ENEMY_COORDINATES': {
             const nextState = prevState.map((enemy, i) => {
                 if (i === index) {
-                    return { ...payload };
+                    return {
+                        ...enemy,
+                        x,
+                        y,
+                    };
+                }
+                return enemy;
+            });
+            return nextState;
+        }
+        case 'MOVE_ENEMY': {
+            const nextState = prevState.map((enemy, i) => {
+                if (i === index) {
+                    return {
+                        ...enemy,
+                        speed,
+                        rotDeg,
+                        rot,
+                        walkFrame,
+                    };
+                }
+                return enemy;
+            });
+            return nextState;           
+        }
+        case 'STOP_ENEMY': {
+            const nextState = prevState.map((enemy, i) => {
+                if (i === index) {
+                    return {
+                        ...enemy,
+                        state: 0,
+                        speed: 0,
+                    };
                 }
                 return enemy;
             });
