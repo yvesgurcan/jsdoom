a280e97236415bac7c46b5d6a50088b4108698fb
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-20 21:56:48 -0700

    remove logs in pause

--- client/src/engine/log/logRemoveEvent.js
+++ client/src/engine/log/logRemoveEvent.js
@@ -3,18 +3,8 @@ import { getState, dispatch } from '../store';
 
 const logRemoveEvent = id => {
     dispatch({ type: 'REMOVE_LOG_EVENT', payload: id });
-    const {
-        log,
-        gameCycle: {
-            delay,
-            paused,
-        },
-    } = getState();
 
-    if (paused) {
-        setTimeout(logRemoveEvent, delay);
-        return false;
-    }
+    const { log } = getState();
 
     const logMessages = getElementById('log');
     logMessages.innerHTML = log.map(item => item.message).join('<br/>');
