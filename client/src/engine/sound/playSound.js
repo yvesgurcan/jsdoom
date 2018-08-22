db92ea9b85e312357ed12408a04c7b0974a59297
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-21 19:53:31 -0700

    sound volume

--- client/src/engine/sound/playSound.js
+++ client/src/engine/sound/playSound.js
@@ -6,14 +6,20 @@ import { getState } from '../store';
 
 export default (filename, volume = 1) => {
     const { sound: { volume: masterVolume } } = getState();
-    if (volume < 0.04 || !filename) {
+    if (masterVolume === 0 || !filename) {
         return false;
     }
     
+    const mixedVolume = Math.min(1, volume * masterVolume);
+
+    if (mixedVolume < 0.045) {
+        return false;
+    }
+
     const path = `${soundPath}/${filename}${sndExt}`;
     const sound = new Audio(path);
-    sound.volume = Math.min(1, volume * masterVolume);
-    console.log({ vol: sound.volume });
+    sound.volume = mixedVolume;
+    console.log({ mixedVolume });
     sound.play();
     return true;
 };
