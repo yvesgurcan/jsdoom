316d3904b1542973352d278273e302b4c9ae1ed7
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-21 21:04:58 -0700

    enemy with mirrored sprites

--- client/src/maps/enemies.js
+++ client/src/maps/enemies.js
@@ -1,7 +1,9 @@
 // see SPRITES.md for details
 
 export default [
-	{ type: 'chaingunner', x: 18, y: 6 },
-	{ type: 'ss', x: 18, y: 8 },
-	{ type: 'cyberdemon', x: 14, y: 12 },
+	{ type: 'archVile', x: 18, y: 8 },
+	{ type: 'revenant', x: 18, y: 7 },
+	{ type: 'spiderMastermind', x: 16, y: 6 },
+	{ type: 'hellKnight', x: 18, y: 8 },
+	{ type: 'arachnotron', x: 14, y: 12 },
 ];
