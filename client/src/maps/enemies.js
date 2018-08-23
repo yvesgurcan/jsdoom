d0c2cc60f60ae9b4ff8dd48fff00e791dcd60b37
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-08-23 00:01:42 -0700

    update map enemies

--- client/src/maps/enemies.js
+++ client/src/maps/enemies.js
@@ -9,7 +9,7 @@ const {
 } = DIRECTIONS;
 
 export default [
-	{ type: 'revenant', x: 18, y: 7, rotDeg: NORTH },
-	{ type: 'archVile', x: 18, y: 10, rotDeg: EAST },
-	{ type: 'baronOfHell', x: 18, y: 11, rotDeg: SOUTH },
+	{ type: 'baronOfHell', x: 18, y: 7, rotDeg: NORTH },
+	// { type: 'archVile', x: 18, y: 10, rotDeg: EAST },
+	// { type: 'baronOfHell', x: 18, y: 11, rotDeg: SOUTH },
 ];
