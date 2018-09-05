512a22901e1ec99e432144dcd1be39ded175972a
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-09-04 22:14:36 -0700

    new constants

--- client/src/engine/resizeView.js
+++ client/src/engine/resizeView.js
@@ -1,12 +1,21 @@
 import { dispatch, getState } from './store';
 import initScreen from './initScreen';
 import initAutomap from './automap/initAutomap';
+import getElementById from './getElementById';
 
-function resizeView() {
+const resizeView = () => {
     const screenWidth = document.body.clientWidth;
     const screenHeight = document.body.clientHeight;
     dispatch({ type: 'SET_VIEW_SIZE', payload: { screenWidth, screenHeight } });
-}
+};
+
+const adjustStatusBarFiller = (state) => {
+    const { constants: { STATUS_BAR_FILLER } } = state;
+    const statusBarBackground = getElementById('statusbar');
+    const statusBarContainer = getElementById('statusbarcontainer');
+    statusBarContainer.style.backgroundImage = `url('${STATUS_BAR_FILLER}')`;
+    statusBarContainer.style.height = statusBarBackground.offsetHeight;
+};
 
 export default () => {
     resizeView();
@@ -14,7 +23,11 @@ export default () => {
 	window.onresize = function () {
         resizeView();
         initScreen();
-        const { automap: { showAutomap } } = getState();
+
+        const state = getState();
+        const { automap: { showAutomap } } = state;
+        adjustStatusBarFiller(state);
+
         if (showAutomap) {
             initAutomap();
         }
