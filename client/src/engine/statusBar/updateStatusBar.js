52a7c0d20537f8aa714eb5a8f62487012b776035
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-09-04 20:35:22 -0700

    rename drawWeapons to drawWeaponSlots

--- client/src/engine/statusBar/updateStatusBar.js
+++ client/src/engine/statusBar/updateStatusBar.js
@@ -2,14 +2,14 @@ import drawMugShot from './drawMugShot';
 import drawHealth from './drawHealth';
 import drawArmor from './drawArmor';
 import drawKeys from './drawKeys';
-import drawWeapons from './drawWeapons';
+import drawWeaponSlots from './drawWeaponSlots';
 import drawSelectedAmmo from './drawSelectedAmmo';
 import drawAmmo from './drawAmmo';
 
 export default (state) => {
     drawSelectedAmmo(state);
     drawHealth();
-    drawWeapons(state);
+    drawWeaponSlots(state);
     drawMugShot();
     drawArmor();
     drawKeys(state);
