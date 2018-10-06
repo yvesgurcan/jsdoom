21f71978f971a984385088ac3906dac5dddf5cde
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-10-06 00:50:33 -0700

    refactor initDecorations and initEnemies with initSprites

--- client/src/engine/decorations/initDecorations.js
+++ client/src/engine/decorations/initDecorations.js
@@ -1,62 +1,20 @@
-import uuid4 from 'uuid4';
-import {
-    imgExt,
-    decorationPath,
-} from '../constants';
+import initSprites from '../initSprites';
 import { dispatch } from '../store';
-import getElementById from '../getElementById';
-import createElement from '../createElement';
 
-export default (state) => {
-    const spriteMap = [];
-    const screen = getElementById('screen');
-    
+export default state => {
     const {
+        constants: { DECORATION_PATH },
         wallMap: map,
-        decorationTypes: itemTypes,
-        decorationMap: mapItems,
+        decorationTypes,
+        decorationMap: decorations,
     } = state;
 
-	for (let y = 0; y < map.length; y++) {
+    const spriteMap = [];
+    for (let y = 0; y < map.length; y++) {
 		spriteMap[y] = [];
 	}
 
-    let decorations = [];
-	for (let i = 0; i < mapItems.length; i++) {
-        const decoration = { ...mapItems[i] };
-		const sprite = { ...decoration };
-        const itemType = itemTypes[sprite.type];
-        if (!itemType) {
-            console.error(`Could not find decoration type '${sprite.type}' for decoration at {x: ${sprite.x}, y: ${sprite.y}}`);
-            /* eslint-disable-next-line */
-            continue;
-        }
-
-        const id = uuid4();
-
-        const img = createElement('img');
-        img.id = id;
-        img.className = sprite.type;
-		img.src = `${decorationPath}/${itemType.img}${imgExt}`;
-		img.style.display = 'none';
-		img.style.position = 'absolute';
-
-		// sprite.visible = false;
-		sprite.block = itemType.block;
-		sprite.img = img;
-
-		spriteMap[sprite.y][sprite.x] = sprite;
-        screen.appendChild(img);
-
-        decorations = [
-            ...decorations,
-            {
-                ...decoration,
-                id,
-            },
-        ];
-    }
-    
-    dispatch({ type: 'INIT_DECORATION_MAP', payload: decorations });
+    const decorationList = initSprites(decorations, decorationTypes, DECORATION_PATH, state, 'decorations', spriteMap);
+    dispatch({ type: 'INIT_DECORATION_MAP', payload: decorationList });
     dispatch({ type: 'PLACE_DECORATIONS', payload: spriteMap });
 };
--- client/src/engine/enemies/initEnemies.js
+++ client/src/engine/enemies/initEnemies.js
@@ -1,105 +1,13 @@
-import uuid4 from 'uuid4';
-import {
-    enemyPath,
-    imgExt,
-} from '../constants';
-import getElementById from '../getElementById';
-import createElement from '../createElement';
+import initSprites from '../initSprites';
+import { dispatch } from '../store';
 
-import { getState, dispatch } from '../store';
-
-export default () => {
-    let enemies = [];
-    const screen = getElementById('screen');
-
+export default state => {
     const {
-        enemyMap: mapEnemies,
+        constants: { ITEM_PATH },
         enemyTypes,
-    } = getState();
+        enemyMap: enemies,
+    } = state;
 
-    for (let i = 0; i < mapEnemies.length; i++) {
-        const enemy = { ...mapEnemies[i] };
-        const {
-            type,
-            x,
-            y,
-        } = enemy;
-
-        if (!type) {
-            console.error('initEnemies(): Enemy type is undefined. Entity will not be placed on the map.', { entity: enemy });
-            /* eslint-disable-next-line */
-            continue;
-        }
-
-        if (!x || !y) {
-            console.error('initEnemies(): x and/or y coordinates are undefined. Entity will not be placed on the map.', { entity: enemy });
-            /* eslint-disable-next-line */
-            continue;
-        }
-        
-        const enemyType = enemyTypes[type];
-
-        if (!enemyType) {
-            console.error(`initEnemies(): Enemy type '${type}' is undefined. Entity will not be placed on the map.`, { entity: enemy, enemyType });
-            /* eslint-disable-next-line */
-            continue;
-        }
-
-        const {
-            prefix,
-            moveSpeed,
-            rotSpeed,
-        } = enemyType;
-
-        if (!prefix) {
-            console.error('initEnemies(): Enemy type is missing a prefix for image filenames. Entity will not be placed on the map.', { entity: enemy, enemyType });
-            /* eslint-disable-next-line */
-            continue;
-        }
-
-        if (!moveSpeed || !rotSpeed) {
-            console.error('initEnemies(): moveSpeed and/or rotSpeed of enemy type is undefined. Entity will not be placed on the map.', { entity: enemy, enemyType });
-            /* eslint-disable-next-line */
-            continue;
-        }
-        
-        const id = uuid4();
-
-        const img = createElement('img');
-        img.id = id;
-        img.className = type;
-        img.src = `${enemyPath}/${enemyType.prefix}/${enemyType.prefix}A1${imgExt}`;
-        img.style.display = 'none';
-        img.style.position = 'absolute';
-
-        const element = {
-            awake: true,
-            rotDeg: 0,
-            rot: 0,
-            ...enemy,
-            id,
-            dir: 0,
-            speed: 0,
-            moveSpeed,
-            rotSpeed,
-            oldStyles: {
-                left: 0,
-                top: 0,
-                width: 0,
-                height: 0,
-                clip: '',
-                display: 'none',
-                zIndex: 0
-            },
-            img,
-        };
-
-        enemies = [
-            ...enemies,
-            element,
-        ];
-
-        screen.appendChild(img);
-    }
-    dispatch({ type: 'INIT_ENEMY_MAP', payload: enemies });
+    const enemyList = initSprites(enemies, enemyTypes, ITEM_PATH, state, 'enemies');
+    dispatch({ type: 'INIT_ENEMY_MAP', payload: enemyList });
 };
