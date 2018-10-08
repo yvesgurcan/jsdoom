fa1287c04cdf037726ef7066d2ebcb67be271105
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-10-07 20:56:27 -0700

    move stuff

--- client/src/maps/enemies.js
+++ client/src/maps/enemies.js
@@ -9,7 +9,7 @@ const {
 } = DIRECTIONS;
 
 export default [
-	{ type: 'imp', x: 12, y: 6, rotDeg: NORTH },
+	{ type: 'imp', x: 8, y: 10, rotDeg: NORTH },
 	// { type: 'imp', x: 18, y: 10, rotDeg: EAST },
 	// { type: 'baronOfHell', x: 18, y: 11, rotDeg: SOUTH },
 ];
