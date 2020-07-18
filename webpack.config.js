34cecc3ef28b29e77cb2f221945ff987fdcf4aaf
yvesgurcan <gurcan.yves+github@gmail.com>
2020-07-17 21:54:28 -0700

    Optimize Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,15 @@ const { GenerateSW } = require('workbox-webpack-plugin');
 
 module.exports = {
     output: {
-        filename: '[name].[hash].js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, './')
     },
     plugins: [
         new CleanWebpackPlugin({
             cleanOnceBeforeBuildPatterns: [
                 'main.*.js',
-                'precache-manifest.*.js'
+                'vendors~main.*.js',
+                'runtime.*.js'
             ]
         }),
         new HtmlWebpackPlugin({
@@ -20,20 +21,21 @@ module.exports = {
             template: './src/index.html'
         }),
         new GenerateSW({
-            skipWaiting: true,
-            clientsClaim: true,
-            runtimeCaching: [
-                {
-                    urlPattern: new RegExp(''),
-                    handler: 'NetworkFirst'
-                }
-            ]
+            skipWaiting: true
         })
     ],
-    watch: true,
+    optimization: {
+        // Separate runtime code into a runtime chunk
+        runtimeChunk: 'single',
+        // Separate dependencies into a vendors chunk
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
     devServer: {
         overlay: true,
-        writeToDisk: true,
-        stats: 'minimal'
+        stats: 'minimal',
+        host: '0.0.0.0',
+        disableHostCheck: true
     }
 };
